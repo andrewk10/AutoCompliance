e0f1ab1fb52751022a4df961f8503a567c0bfe4b
andrewk10 <19553807+andrewk10@users.noreply.github.com>
2022-01-10 17:32:52 +0000

    Started OOP Refactoring, Test Creation and PyCharm Project Files
    Initial commit to have the pull request available on GitHub

---
+++ test_main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import main
+"""
+===PLEASE READ===
+Functions are organised alphabetically with the main function specified last.
+Every function besides the main function has a block comment explaining what it
+does, the main function itself has more specific, low level commenting.
+"""
+
+
+class TestMain:
+    def test_file_error_handler():
+        """
+        This function tests the file_error_handler method in the main class.
+        Should just run straight through no problem hence why all this method
+        does is run that method, errors or exceptions will fail this test for
+        us.
+        """
+        main.file_error_handler("test")
