802de688f161cb33edfc88ef2c3c6aa5a16b9d66
andrewk10 <19553807+andrewk10@users.noreply.github.com>
2022-03-22 22:03:17 +0000

    [UPDATE] Run demo.sh updated with example inputs
    Need to refine the demo.py script now

--- run_demo.sh
+++ run_demo.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-# Demo run for demo.py
+# Running the propagation script across numerous services (SSH, Telnet, Web)
+./src/demo.py -t src/test_files/ip_list.txt -p 22,23,25,80 -u admin -f src/test_files/passwords_list.txt
+
+# Running the propagation script just across SSH
+./src/demo.py -t src/test_files/ip_list.txt -p 22 -u root -f src/test_files/passwords_list.txt
+
+# Running the propagation script just across SSH and spreading a specific file.
+./src/demo.py -t src/test_files/ip_list.txt -p 22 -u root -f src/test_files/passwords_list.txt -d src/test_files/file.txt
+
+# Running the propagation script across SSH and Telnet but acquiring IPs through a local scan and then subsequently self propagating.
+./src/demo.py -L -p 22,23 -u root -f src/test_files/passwords_list.txt -P
