060dfb7a79f5929c639f118a9706337be89afd20
andrewk10 <19553807+andrewk10@users.noreply.github.com>
2022-03-23 15:42:13 +0000

    [UPDATE] Short test files, demo.sh changes

--- demo.sh
+++ demo.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
+# For demo purposes only, runs demo.py
+
 # Running the propagation script across numerous services (SSH, Telnet, Web)
-./src/demo.py -t src/test_files/ip_list.txt -p 22,23,25,80 -u admin -f src/test_files/passwords_list.txt
+echo "Running the propagation script across numerous services (SSH, Telnet, Web)"
+./src/demo.py -t src/test_files/ip_list_short.txt -p 22,25,80 -u admin -f src/test_files/passwords_list_short.txt
 
 # Running the propagation script just across SSH
-./src/demo.py -t src/test_files/ip_list.txt -p 22 -u root -f src/test_files/passwords_list.txt
+echo "Running the propagation script just across SSH"
+./src/demo.py -t src/test_files/ip_list_short.txt -p 22 -u root -f src/test_files/passwords_list_short.txt
 
 # Running the propagation script just across SSH and spreading a specific file.
-./src/demo.py -t src/test_files/ip_list.txt -p 22 -u root -f src/test_files/passwords_list.txt -d src/test_files/file.txt
+echo "Running the propagation script just across SSH and spreading a specific file"
+./src/demo.py -t src/test_files/ip_list_short.txt -p 22 -u root -f src/test_files/passwords_list_short.txt -d src/test_files/file.txt
 
 # Running the propagation script across SSH and Telnet but acquiring IPs through a local scan and then subsequently self propagating.
-./src/demo.py -L -p 22,23 -u root -f src/test_files/passwords_list.txt -P
+echo "Running the propagation script across SSH and Telnet but acquiring IPs through a local scan and then subsequently self propagating."
+./src/demo.py -L -p 22,23 -u root -f src/test_files/passwords_list_short.txt -P
