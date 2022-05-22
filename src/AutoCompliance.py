c2771bc8c01c95447a8253bd0d0013a22d896581
Andrew <19553807+andrewk10@users.noreply.github.com>
2022-05-22 12:41:57 +0100

    Revert "[UPDATE] Requirements.txt for pyGobject and default UI class added"
    This reverts commit 75b8388008f20e50c27a1a5af0c2906abf828e52.

--- src/AutoCompliance.py
+++ src/AutoCompliance.py
@@ -1,13 +1,9 @@
-
 import gi
-
 gi.require_version("Gtk", "4.0")
-from gi.repository import Gtk
 
 
 def on_activate(app):
     win = gi.repository.Gtk.ApplicationWindow(application=app)
-    win.set_name("AutoCompliance")
     btn = gi.repository.Gtk.Button(label="Hello, World!")
     btn.connect('clicked', lambda x: win.close())
     win.set_child(btn)
